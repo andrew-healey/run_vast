3a0f3f8e2f6942b65a82224cae818ba390214e1a
Andrew Healey <doolie.healey@gmail.com>
2025-04-02 17:54:50 -0700

    old license changes, iirc I needed them to fix pypi publishing

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/run_vast",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
